f81ccb5669153cb38c224053e1eaec69530fb1e9
Sibinvmsibin <webdevelopersibin@gmail.com>
2023-02-19 16:27:46 +0530

    list files from drive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $.ajax({
     url : "http://127.0.0.1:5000/list-files",
     success : function(response) {
-        console.log(response);
+        $.each(response, function(item) {
+            console.log(item);
+        });
     }
-} )
+});
